186136ed86030648fb63ae62e625e28a8c911ec8
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-10-10 09:06:19 +0200

    Filtering & sorting products

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,9 +11,28 @@ builder.Services.AddDbContext<StoreContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
 app.MapControllers();
+
+try
+{
+    // so we can dispose the services outside the scope of injection...
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<StoreContext>();
+    // applies any pending migrations for the context to the database. Will create the database if it does not already exist.
+    await context.Database.MigrateAsync();
+    
+    // Seeding data
+    await StoreContextSeed.SeedAsync(context);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    throw;
+}
 
 app.Run();
