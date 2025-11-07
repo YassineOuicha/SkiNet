2f4a14650d6841f063f1be5c14b77f2aa69c7385
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-11-07 19:27:25 +0100

    Configuring redis to save shopping cart

---
+++ Core/Interfaces/ICartService.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entities;
+
+namespace Core.Interfaces;
+
+// we don't use repository naming because doesn't interact with EF Core, but Redis
+public interface ICartService
+{
+    Task<ShoppingCart?> GetCartAsync(string cartId);
+    Task<ShoppingCart?> SetCartAsync(ShoppingCart cart);
+    Task<bool> DeleteCartAsync(string cartId);
+}
