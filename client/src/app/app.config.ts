ed4d83bd5d7cb1d147719ff2caca563cd479cbaa
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-11-09 13:07:33 +0100

    Cart features completed

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig, inject, provideAppInitializer, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
@@ -6,6 +6,9 @@ import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {errorInterceptor} from './core/interceptors/error.interceptor';
 import {loadingInterceptor} from './core/interceptors/loading.interceptor';
+import {InitService} from './core/services/init.service';
+import {lastValueFrom} from 'rxjs';
+import {MAT_DIALOG_DEFAULT_OPTIONS} from '@angular/material/dialog';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,6 +18,19 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptors([
       errorInterceptor,
       loadingInterceptor
-    ]))
+    ])),
+    provideAppInitializer(async () => {
+      const initService = inject(InitService);
+      return lastValueFrom(initService.init()).finally(() => {
+        const splash = document.getElementById('initial-splash');
+        if (splash) {
+          splash.remove();
+        }
+      });
+    }),
+    {
+      provide: MAT_DIALOG_DEFAULT_OPTIONS,
+      useValue: {autoFocus: 'dialog', restoreFocus: true}
+    }
   ]
 };
--- client/src/app/features/product-details/product-details.component.ts
+++ client/src/app/features/product-details/product-details.component.ts
@@ -7,10 +7,12 @@ import {MatButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
 import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
 import {MatDivider} from '@angular/material/divider';
+import {CartService} from '../../core/services/cart.service';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-product-details',
-  imports: [CurrencyPipe, MatButton, MatIcon, MatFormField, MatLabel, MatInput, MatDivider],
+  imports: [CurrencyPipe, MatButton, MatIcon, MatFormField, MatLabel, MatInput, MatDivider, FormsModule],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss'
 })
@@ -18,6 +20,9 @@ export class ProductDetailsComponent implements OnInit {
   private shopService = inject(ShopService);
   private activatedRoute = inject(ActivatedRoute);
   product?: Product;
+  quantityInCart = 0;
+  quantity = 1;
+  private cartService = inject(CartService);
 
   ngOnInit(): void {
     this.loadProduct();
@@ -27,8 +32,34 @@ export class ProductDetailsComponent implements OnInit {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
     if (!id) return;
     this.shopService.getProduct(+id).subscribe({
-      next: product => this.product = product,
+      next: product => {
+        this.product = product
+        this.updateQuantityInCart();
+      },
       error: error => console.log(error)
     });
+  }
+
+  updateQuantityInCart() {
+    this.quantityInCart = this.cartService.cart()?.items
+      .find(x => x.productId === this.product?.id)?.quantity || 0;
+    this.quantity = this.quantityInCart || 1;
+  }
+
+  getButtonText(): string {
+    return this.quantityInCart === 0 ? 'Add to cart' : 'Update cart';
+  }
+
+  updateCart() {
+    if (!this.product) return;
+    if (this.quantity > this.quantityInCart) {
+      const itemsToAdd = this.quantity - this.quantityInCart;
+      this.quantityInCart += itemsToAdd;
+      this.cartService.addItemToCart(this.product, itemsToAdd);
+    } else {
+      const itemsToRemove = this.quantityInCart - this.quantity;
+      this.quantityInCart -= itemsToRemove;
+      this.cartService.removeItemFromCart(this.product.id, itemsToRemove);
+    }
   }
 }
