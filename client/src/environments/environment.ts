013008cbc9931711965fb0f0286e5fdefe2be6af
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-11-22 14:44:11 +0100

    Updating stripe prod secret

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'api/',
   hubUrl: 'hub/notifications',
-  stripePublicKey: 'pk_test_51SSH5jGXLou0F6hxclNw16FYnbxWveQHJQW71U53eYb9nBXmTrdar8z7K5MF8ijhcN7PxFGEUeUUCmu8aAKFF4Vk00nRXvHQUy'
+  stripePublicKey: 'pk_test_51SVvkM2cauWtznH4NBvNar8RkAFsybXCRnwBILBFY8tu0VjeIVjojQNM0Sx23eiHGIWQRJPjLUoLzkUh8pvDTlK100tCgUb3ws'
 };
